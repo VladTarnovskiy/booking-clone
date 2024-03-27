14a5ea15426cb77c2d7d1c23e7722f1ecd83ab35
vladtarnovskiy <vladtarnovski989@gmail.com>
2024-03-27 11:17:59 +0300

    feat: configure project

---
+++ booking-clone/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        base_blue: '#3b82f6',
+      },
+    },
+  },
+  plugins: [],
+};
