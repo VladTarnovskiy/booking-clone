f1088953d0824d8be0979c3a63216d88f3586e07
vladtarnovskiy <vladtarnovski989@gmail.com>
2024-04-01 12:51:00 +0300

    refactor: filter choose

--- booking-clone/src/app/pages/booking-layout/booking-layout.component.ts
+++ booking-clone/src/app/pages/booking-layout/booking-layout.component.ts
@@ -1,8 +1,10 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AsyncPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { NavigationStart, Router, RouterOutlet } from '@angular/router';
 import { CarsFilterComponent } from '@components/cars/cars-filter';
 import { NavigationComponent } from '@components/core/navigation';
 import { StaysFilterComponent } from '@components/stays/stays-filter';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-booking-layout',
@@ -12,9 +14,21 @@ import { StaysFilterComponent } from '@components/stays/stays-filter';
     NavigationComponent,
     CarsFilterComponent,
     StaysFilterComponent,
+    AsyncPipe,
   ],
   templateUrl: './booking-layout.component.html',
   styleUrl: './booking-layout.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BookingLayoutComponent {}
+export class BookingLayoutComponent implements OnInit {
+  currentRoute = new BehaviorSubject<string>(this.router.url);
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        this.currentRoute.next(event.url);
+      }
+    });
+  }
+}
