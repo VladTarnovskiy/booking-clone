bcb42c36f33a0a8cf76e38d41e09c7698d6c307f
vladtarnovskiy <vladtarnovski989@gmail.com>
2024-04-15 01:50:14 +0300

    refactor: album adaptive

--- booking-clone/src/app/components/core/navigation/constants.ts
+++ booking-clone/src/app/components/core/navigation/constants.ts
@@ -1,5 +1,5 @@
 export const links = [
-  { title: 'Car', link: '/booking/cars', image: 'assets/car.svg' },
+  { title: 'Cars', link: '/booking/cars', image: 'assets/car.svg' },
   { title: 'Stays', link: '/booking/stays', image: 'assets/home.svg' },
   { title: 'Flights', link: '/booking/flights', image: 'assets/jet.svg' },
   {
